d11f2c1574e4cf8ce6327e549bfcc31b4cd2975c
Dmitriy <levchenko.d.a1998@gmail.com>
2021-03-26 08:29:16 +0300

    .

--- handlers/dialog.py
+++ handlers/dialog.py
@@ -14,19 +14,21 @@ async def ignore_msg(*_):
     pass
 
 
-@dp.message_handler(find_pair_chat, user_id=GROUP_ADMIN_ID)
+@dp.message_handler(find_pair_chat, content_types='any', user_id=GROUP_ADMIN_ID)
 async def forward_from_admin(msg: types.Message, pchat_id: int):
     await msg.forward(pchat_id)
 
 
-@dp.message_handler(find_pair_chat)
+@dp.message_handler(find_pair_chat, content_types='any')
 async def forward(msg: types.Message, pchat_id: int):
     """Копирует все сообщения в связанную группу."""
     username = msg.from_user.username
     lower_msg_text = msg.text.lower()
-    stop_words = ['cозвон', 'телефон', 'вайбер', 'ватсап', 'whatsapp', 'мессенджер',
-                  'e-mail', 'почта', 'электронка', 'напрямую', 'ссылка', 'как найти тебя',
-                  'телега', 'как найти тебя', 'telegram', '@', 't.me']
+    stop_words = [
+        'cозвон', 'телефон', 'вайбер', 'ватсап', 'whatsapp', 'мессенджер',
+        'e-mail', 'почта', 'электронка', 'напрямую', 'ссылка', 'как найти тебя',
+        'телега', 'как найти тебя', 'telegram', '@', 't.me'
+    ]
 
     for word in stop_words:
         if word in lower_msg_text:
--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 """Main script, starts bot in long-polling mode."""
 
-from aiogram import executor
+from aiogram import executor, Dispatcher
 
 from loader import dp
 
 
-async def on_startup(*args):
+async def on_startup(*_):
     import logging
     import handlers
     import filters
@@ -16,5 +16,10 @@ async def on_startup(*args):
     logger.info('Import %s', filters)
 
 
+async def on_shutdown(dispatcher: Dispatcher):
+    await dispatcher.storage.close()
+    await dispatcher.storage.wait_closed()
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+    executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
